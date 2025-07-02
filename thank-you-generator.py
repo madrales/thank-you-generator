89f52adb01673b1c36fd6afa2c2b6791d9eb37de
Mason Adrales <masonadrales@Masons-MacBook-Pro.local>
2025-07-02 14:13:45 -0500

    cleaning code and changing verbiage

--- thank-you-generator.py
+++ thank-you-generator.py
@@ -1,19 +1,12 @@
 f = open("gifts.txt")
-x = f.readlines()
-
-# print(x)
-y = ", ".join(x)
-
-# print(y)
-z = y.split(",")
-
-
-# print(z)
 n = open("notes.txt", "w")
+x = f.readlines()
+y = ", ".join(x)
+z = y.split(",")
 try:
     for i in z:
         k = i.split(":")
-        template = f'\n\n\n\n{k[0]},\n\nThank you so much for the{k[1]}!\n\nWe loved seeing you at the baby shower and are very excited for you to meet our baby girl in September. We are very grateful to have you in our lives and appreciate the time you took to celebrate with us at the shower.\n\nThank you again for your generosity! Sending our best wishes for you and your family,\n\nMason, Bailey, and Baby Rose Adrales'
+        template = f'\n\n\n\n{k[0]},\n\nWe wanted to reach out and say thank you so much for the{k[1]}!\n\nWe loved seeing you at the baby shower and are very excited for you to meet our baby girl in September. We are very grateful to have you in our lives and appreciate the time you took to celebrate with us at the shower.\n\nThank you again for your generosity!'
         print(template)
         n.write(template)
 except IndexError:
